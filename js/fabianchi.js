3107ff2288b27a5f021dedf5c4c74bb3e3b76708
Fabian <fabianalfonsocv@gmail.com>
2016-09-18 22:35:58 -0500

    added my work section, added card with qr code

--- js/fabianchi.js
+++ js/fabianchi.js
@@ -10,6 +10,19 @@ $(document).ready(function(){
 	//$('#fullpage').fullpage();
 });
 
+/*$(document).on('ready', function(){
+	var btn = $('#btn-registrarse');
+	var btnJsonStyle = {'background-color':'#2F2C6D', 'color':'#dedede'}
+	var claseObtenida = $(btn).attr('class');
+	alert(claseObtenida);
+
+	if($(btn).has('class', 'teal') ){
+		$(btn).removeClass('teal', 'darken-4');
+		$(btn).css(btnJsonStyle);
+	};	
+
+	$(btn).text('Registro');
+});*/
 
 
 function carouselAutoplay() {
@@ -25,12 +38,12 @@ var options = {
   separator : ',', 
   decimal : '.', 
   prefix : '', 
-  suffix : '' 
+  suffix : '',
 };
-var projects = new CountUp("proyectos", 0, 8, 0, 5, options);
-var hours = new CountUp("horas", 0, 350, 0, 5, options);
+var projects = new CountUp("proyectos", 0, 8, 0, 6, options);
+var hours = new CountUp("horas", 0, 350, 0, 6, options);
 var code = new CountUp("codigo", 0, 8500, 0, 7, options);
-var years = new CountUp("años", 0, 1.5, 1, 5, options);
+var years = new CountUp("años", 0, 1.5, 1, 6, options);
 
 
 //Scroll fire-----------------------------------------
@@ -43,5 +56,17 @@ var sfOptions = [
 		years.start();
     	} 
 	}
+/*
+	,
+	{selector: '.about', offset:20, callback: function()
+		{
+			$('.about').addClass("slideExpandUp");
+		}
+	},
+	{selector: '.experience', offset:20, callback: function()
+		{
+			$('.experience').addClass("slideExpandUp");
+		}
+	}
+	*/
   ];
- 
